6475b130ffa0931442deb0b38d4052746421c8c2
soumil <soumilbaldot@gmail.com>
2022-06-25 11:55:48 +0530

    testing types instead of strings

--- src/parse_type.jl
+++ src/parse_type.jl
@@ -1,5 +1,4 @@
 julia_type = Dict(
-
 	"float" => "Float32" ,
 	"double" => "Float64" ,
 	"bool" => "Bool" ,
@@ -12,28 +11,13 @@ julia_type = Dict(
 	"uint32_t" => "UInt32" ,
 	"int64_t" => "Int64" ,
 	"uint64_t" => "UInt64" ,
+	"std::array<float,6>" => "MVector{6, Float32}" , 
+	"std::array<float,21>" => "MVector{21, Float32}" , 
 	)
-function get_julia_type(s::String)
 
+function get_julia_type(s::String)
 	return julia_type[s]
 end
 
-# we can return the julia type as "Type" also
-
-# julia_type = Dict(
-
-# 	"float" => Float32,
-# 	"double" => Float64,
-# 	"bool" => Bool,
-# 	"int" => Int,
-# 	"int8_t" => Int8,
-# 	"uint8_t" => UInt8,
-# 	"int16_t" => Int16,
-# 	"uint16_t" => UInt16,
-# 	"int32_t" => Int32,
-# 	"uint32_t" => UInt32,
-# 	"int64_t" => Int64,
-# 	"uint64_t" => UInt64,
-# 	)
 
 
--- test/parsed_julia_type.jl
+++ test/parsed_julia_type.jl
@@ -1,10 +1,19 @@
+using StaticArrays
+
 include("../src/parse_type.jl")
-
-cpp_types=["double", "bool", "int", "float", "int8_t", "uint8_t", "int16_t", "uint16_t", "int32_t", "uint32_t", "int64_t", "uint64_t"]
-
-for type in cpp_types
-	jl_string = get_julia_type(type)
-	parsed_symbol = eval(Meta.parse(jl_string))(0)
-	@test repr(typeof(parsed_symbol)) == jl_string
-end
-
+@test eval(Meta.parse(get_julia_type("double"))) == Float64
+@test eval(Meta.parse(get_julia_type("bool"))) == Bool
+@test eval(Meta.parse(get_julia_type("int"))) == Int64
+@test eval(Meta.parse(get_julia_type("float"))) == Float32
+@test eval(Meta.parse(get_julia_type("int8_t"))) == Int8
+@test eval(Meta.parse(get_julia_type("uint8_t"))) == UInt8
+@test eval(Meta.parse(get_julia_type("int16_t"))) == Int16
+@test eval(Meta.parse(get_julia_type("uint16_t"))) == UInt16
+@test eval(Meta.parse(get_julia_type("int32_t"))) == Int32
+@test eval(Meta.parse(get_julia_type("uint32_t"))) == UInt32
+@test eval(Meta.parse(get_julia_type("int64_t"))) == Int64
+@test eval(Meta.parse(get_julia_type("uint64_t"))) == UInt64
+@test eval(Meta.parse(get_julia_type("std::array<float,6>"))) == MArray{Tuple{6}, Float32, 1, 6}
+@test eval(Meta.parse(get_julia_type("std::array<float,6>"))) == MVector{6,Float32}
+@test eval(Meta.parse(get_julia_type("std::array<float,21>"))) == MArray{Tuple{21}, Float32, 1, 21}
+@test eval(Meta.parse(get_julia_type("std::array<float,21>"))) == MVector{21,Float32}
